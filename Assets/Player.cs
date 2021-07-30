11b65b9984c71e1725c81985f30b2f0a58bc7183
kksumm123 <kksumm123@gmail.com>
2021-07-30 20:41:40 +0900

    1. Player 기본적인 움직임 구현완료 2. 도로에 Static체크, Nav Bake

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] float speed = 5;
+    float lookatRotationValue = 0.05f;
 
+
+    Vector3 move;
     void Update()
     {
-        
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) move.z = 1;
+        if (Input.GetKey(KeyCode.S)) move.z = -1;
+        if (Input.GetKey(KeyCode.A)) move.x = -1;
+        if (Input.GetKey(KeyCode.D)) move.x = 1;
+
+        if (move != Vector3.zero)
+        {
+            move.Normalize();
+            transform.Translate(speed * Time.deltaTime * move, Space.World);
+            transform.forward = Vector3.Slerp(transform.forward, move, lookatRotationValue);
+        }
     }
 }
