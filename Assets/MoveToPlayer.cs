eaa36196a7275e5a943d02cac3a4b22aa566cc9f
kksumm123 <kksumm123@gmail.com>
2021-08-05 16:23:25 +0900

    1. DOTween 설치 2. Coin 애니메이션, 콜라이더 추가 3. MoveToPlayer.cs 완성

--- Assets/MoveToPlayer.cs
+++ Assets/MoveToPlayer.cs
@@ -2,21 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using DG.Tweening;
 
 public class MoveToPlayer : MonoBehaviour
 {
     NavMeshAgent agent;
     [SerializeField] float maxSpeed = 20;
     [SerializeField] float duration = 3;
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-    }
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //agent.speed
+            agent = GetComponent<NavMeshAgent>();
+
+            DOTween.To(() => 1f, (x) => agent.speed = x, maxSpeed, duration);
         }
     }
 }
