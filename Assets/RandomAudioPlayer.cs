8eb437c4e4ea5c84d5c2de435a45bbfadbb4c695
kksumm123 <kksumm123@gmail.com>
2021-08-12 21:37:21 +0900

    1. RandomAudioPlayer.cs 수정 2. 환경음 추가

--- Assets/RandomAudioPlayer.cs
+++ Assets/RandomAudioPlayer.cs
@@ -10,10 +10,13 @@ public class RandomAudioPlayer : MonoBehaviour
     {
         public AudioClip clip;
         public float ratio;
+        public float minVolume = 1f;
+        public float maxVolume = 1f;
     }
     [SerializeField] List<AudioInfo> audios;
     [SerializeField] float maxRandomTime = 30;
-
+    [SerializeField] float gMinVolume = 0.5f;
+    [SerializeField] float gMaxVolume = 1f;
     IEnumerator Start()
     {
         AudioSource audioSource = GetComponent<AudioSource>();
@@ -21,11 +24,14 @@ public class RandomAudioPlayer : MonoBehaviour
         {
             while (true)
             {
-                audioSource.clip = audios.OrderBy(x => Random.Range(0, x.ratio))
-                                         .Last().clip;
+                yield return new WaitForSeconds(Random.Range(0, maxRandomTime));
+
+                var info = audios.OrderBy(x => Random.Range(0, x.ratio)).Last();
+                audioSource.volume = Random.Range(info.minVolume * gMinVolume, info.maxVolume * gMaxVolume);
+                audioSource.clip = info.clip;
                 audioSource.Play();
 
-                yield return new WaitForSeconds(audioSource.clip.length + Random.Range(0, maxRandomTime));
+                yield return new WaitForSeconds(audioSource.clip.length);
             }
         }
     }
