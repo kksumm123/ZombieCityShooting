89841fd919f59fb62044c21c98a164b5c2446eed
kksumm123 <kksumm123@gmail.com>
2021-08-12 16:47:04 +0900

    1. Grenade.cs 작성 완료 2. ProjectileArc의 라인 렌더러, 매테리얼 값 수정(Width, TilingOffset)

--- Assets/Grenade.cs
+++ Assets/Grenade.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    // Start is called before the first frame update
+    ProjectileArc projectileArc;
+    public Cursor cursor;
+    [SerializeField] float speed = 20;
     void Start()
     {
-        
+        projectileArc = GetComponent<ProjectileArc>();
+        firePoint = transform;
+        cursor = FindObjectOfType<Cursor>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        SetTargetWithSpeed(cursor.transform.position, speed);
+    }
+
+    public Transform firePoint;
+    public float currentAngle;
+    public void SetTargetWithSpeed(Vector3 point, float speed)
+    {
+        //currentSpeed = speed;
+
+        Vector3 direction = point - firePoint.position;
+        float yOffset = direction.y;
+        direction = Math3d.ProjectVectorOnPlane(Vector3.up, direction);
+        float distance = direction.magnitude;
+
+        float angle0, angle1;
+        bool targetInRange = ProjectileMath.LaunchAngle(speed, distance, yOffset, Physics.gravity.magnitude, out angle0, out angle1);
+
+        if (targetInRange)
+            currentAngle = angle0;
+
+        projectileArc.UpdateArc(speed, distance, Physics.gravity.magnitude, currentAngle, direction, targetInRange);
+        //SetTurret(direction, currentAngle * Mathf.Rad2Deg);
+
+        //currentTimeOfFlight = ProjectileMath.TimeOfFlight(currentSpeed, currentAngle, -yOffset, Physics.gravity.magnitude);
     }
 }
