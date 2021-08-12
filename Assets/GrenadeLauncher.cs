fe1593885f4fa46d0d5004b1a7232cc46c8fdd81
kksumm123 <kksumm123@gmail.com>
2021-08-12 17:47:47 +0900

    1. Grenade -> GrenadeLauncher.cs 이름변경 2. 포물선 운동 구현

--- Assets/GrenadeLauncher.cs
+++ Assets/GrenadeLauncher.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grenade : MonoBehaviour
+public class GrenadeLauncher : MonoBehaviour
 {
     ProjectileArc projectileArc;
     public Cursor cursor;
-    [SerializeField] float speed = 20;
+    [SerializeField] float speed = 10;
     void Start()
     {
         projectileArc = GetComponent<ProjectileArc>();
@@ -14,19 +14,33 @@ public class Grenade : MonoBehaviour
         cursor = FindObjectOfType<Cursor>();
     }
 
-
     void Update()
     {
         SetTargetWithSpeed(cursor.transform.position, speed);
+
+        // 수류탄 발사 구현
+        if (Input.GetMouseButtonDown(1))
+        {
+            // 수류탄 생성
+            // 리지드 바디에 포스 줘서 날리자
+            var newGo = Instantiate(grenadeGo, firePoint.position, Quaternion.identity);
+            newGo.transform.forward = direction;
+            float degree = -currentAngle * Mathf.Rad2Deg; // !앵글 -주의
+            newGo.transform.Rotate(degree, 0, degree); // y각도를 위해 x, z 회전
+            var newGoRigid = newGo.GetComponent<Rigidbody>();
+            newGoRigid.velocity = newGo.transform.forward * speed;
+        }
     }
 
+    public GameObject grenadeGo;
     public Transform firePoint;
     public float currentAngle;
+    Vector3 direction;
     public void SetTargetWithSpeed(Vector3 point, float speed)
     {
         //currentSpeed = speed;
 
-        Vector3 direction = point - firePoint.position;
+        direction = point - firePoint.position;
         float yOffset = direction.y;
         direction = Math3d.ProjectVectorOnPlane(Vector3.up, direction);
         float distance = direction.magnitude;
