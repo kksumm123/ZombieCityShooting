5780472a6435a9351939a2596ea770467deb7e2f
kksumm123 <kksumm123@gmail.com>
2021-08-12 18:05:53 +0900

    1. Zombie들에 Attackable 레이어 추가 2. Grenade 데미지 입히는 것까지 완료 3. Zombie의 AttackRange 레이어 디폴트로 수정

--- Assets/Grenade.cs
+++ Assets/Grenade.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    
+    [SerializeField] int power = 50;
+    [SerializeField] float damageArea = 5;
+    [SerializeField] float knockBackForce = 5;
+    [SerializeField] float destroyDelay = 3;
+    GameObject destroyEffect;
+    LayerMask attackableLayer;
+
+    IEnumerator Start()
+    {
+        attackableLayer = 1 << LayerMask.NameToLayer("Attackable");
+        yield return new WaitForSeconds(destroyDelay);
+        // 좀비들에게 데미지 주자
+
+        var attackables = Physics.OverlapSphere(transform.position, damageArea, attackableLayer);
+        foreach (var item in attackables)
+        {
+            transform.LookAt(item.transform.position);
+            item.GetComponent<Zombie>().TakeHit(power, transform, knockBackForce);
+        }
+
+        // 폭발 이펙트 표시
+        //Instantiate(destroyEffect, transform.position, Quaternion.identity);
+    }
 }
