5c1ca26f057e17657727d0604e277afbdf39a9b7
kksumm123 <kksumm123@gmail.com>
2021-08-13 18:00:32 +0900

    GameResultUI.cs 초안 작성 완료

--- Assets/GameResultUI.cs
+++ Assets/GameResultUI.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameResultUI : MonoBehaviour
+public class GameResultUI : SingletonMonoBehavior<GameResultUI>
 {
-    
+
+    Image star;
+    TextMeshProUGUI yourScoreValue;
+    TextMeshProUGUI highScoreValue;
+    protected override void OnInit()
+    {
+        star = transform.Find("Star").GetComponent<Image>();
+        yourScoreValue = transform.Find("YourScore/Value").GetComponent<TextMeshProUGUI>();
+        highScoreValue = transform.Find("HighScore/Value").GetComponent<TextMeshProUGUI>();
+    }
 }
